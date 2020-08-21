2156d2192e36796b5c2806160d57b8b5c9f364bc
Gabriel Giro Resende <gabriel.giro.resende@outlook.com.br>
2020-08-21 14:38:39 -0300

    Changed how we substitute nulls

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -19,27 +19,25 @@ class TratamentoNulos(BaseEstimator, TransformerMixin):
     def __init__(self, desvio=1):
         # Recebe o numero de vezes que vai ser aplicado no desvio padrao, para somar a media
         self.desvio = desvio
+        self.variavies_com_nulos = 0
         
     def fit(self, df, y=None):
         df_analise_nulos = df.isna().sum()
         self.variaveis_com_nulos = df_analise_nulos[df_analise_nulos > 0].index
-        return self.variaveis_com_nulos
-    
-    def SubValue_const(self, x, const, argumento, var_nula):
-        if pd.isna(x[var_nula]) and x["PERFIL"] == argumento:
-            x[var_nula] = const
-        return x
-    
+        return self
+
     def transform(self, df):
-        perfis = ["EXATAS", "HUMANAS", "DIFICULDADE", "MUITO_BOM", "EXCELENTE"]
         if len(self.variaveis_com_nulos) > 0:
             i = 0
             while i < len(self.variaveis_com_nulos):
-                print("\n\nSubstituindo nulos na variável: ", self.variaveis_com_nulos[i], "\n\n")
-                for k in perfis:
-                    media = (df[df.PERFIL == k][self.variaveis_com_nulos[i]].mean() + 
-                             df[df.PERFIL == k][self.variaveis_com_nulos[i]].std()*(self.desvio))
-                    print("\n Substituindo nulos por: ", media, " no perfil: ", k)
-                    df = df.apply(self.SubValue_const, args=(media, k, self.variaveis_com_nulos[i]), axis=1)
+                media = (df[self.variaveis_com_nulos[i]].mean() + 
+                         df[self.variaveis_com_nulos[i]].std()*(self.desvio))
+                print("\n\nSubstituindo nulos na variável: ", self.variaveis_com_nulos[i], " por: ", media, "\n\n")
+                df = df.apply(self.SubValue_const, args=(media, self.variaveis_com_nulos[i]), axis=1)
                 i += 1
         return df
+    
+    def SubValue_const(self, x, const, var_nula):
+        if pd.isna(x[var_nula]):
+            x[var_nula] = const
+        return x
